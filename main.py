2d74e1433ac3e0828519db69908f8de9cb0265b9
DavisGilmore <davisgilmore@yahoo.com>
2017-02-10 14:40:17 -0500

    More things to test - Davis

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ connection = pypyodbc.connect('DRIVER={SQL Server};'
 							  'DATABASE=Chew;'
                               'UID=emelyewu;PWD=Chew$erver15only$erver')
 
-							  
+
 @app.route('/')
 @app.route('/index')
 def welcome():
 	return render_template('index.html')
-	
-	
+
+
 @app.route('/recipe')
 def display_recipe():
 	recipeID = request.args.get('id')
@@ -27,9 +27,18 @@ def display_recipe():
 	query = ("SELECT * FROM Step WHERE RecipeID=" + recipeID)
 	cursor.execute(query)
 	stepsR = cursor.fetchall()
-	#TODO: add new template
 	return render_template('recipe.html', recipe=recipeR[0], steps=stepsR)
-	
-	
+
+
+@app.route('/results')
+def search_results():
+	searchString = request.args.get('for')
+	cursor = connection.cursor()
+	query = ("SEARCH('" + searchString + "')") #TODO put in SPROC
+	cursor.execute(query)
+	matches = cursor.fetchall()
+	return render_template('results.html, recipes=matches)
+
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8080, debug=True)
