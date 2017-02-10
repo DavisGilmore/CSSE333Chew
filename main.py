db115b0756234ba462fc4bc1a9065fb3fdbddf6d
DavisGilmore <davisgilmore@yahoo.com>
2017-02-10 12:08:21 -0500

    testing query - Davis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import flask
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template, jsonify, url_for, redirect
 import pypyodbc
 
 app = Flask(__name__)
@@ -14,6 +14,21 @@ connection = pypyodbc.connect('DRIVER={SQL Server};'
 @app.route('/index')
 def welcome():
 	return render_template('index.html')
+	
+	
+@app.route('/Recipe')
+def display_recipe():
+	recipeID = request.args.get('id')
+	cursor = connection.cursor()
+	#TODO: replace with SPROC
+	query = ("SELECT * FROM Recipe WHERE ID=" + recipeID)
+	cursor.execute(query)
+	recipe = cursor.fetchall()
+	query = ("SELECT * FROM Step WHERE ID=" + recipeID)
+	cursor.execute(query)
+	steps = cursor.fetchall()
+	#TODO: add new template
+	return render_template('recipe.html')
 	
 	
 if __name__ == '__main__':
